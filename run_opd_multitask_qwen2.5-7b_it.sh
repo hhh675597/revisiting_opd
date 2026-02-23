f02a702fecf32c76ca271d2709a613808f1d7d12
hhh675597 <huanghaohuan24@mails.ucas.ac.cn>
2026-02-23 23:58:11 +0800

    fix: multi ref model load err

--- run_opd_multitask_qwen2.5-7b_it.sh
+++ run_opd_multitask_qwen2.5-7b_it.sh
@@ -13,7 +13,7 @@ export RAY_worker_register_timeout_seconds=600
 
 TIME_STAMP=$(date +"%m%d_%H%M%S")
 project_name='multitask_opd'
-exp_name='test_multitask_lr1e-6_k32_normto1_think'
+exp_name='multitask_original_opd_lr2e-6'
 
 set -x
 ENGINE=${1:-vllm}
@@ -30,7 +30,7 @@ group_size=8
 
 MULTITASK_DATA_DIR="/data/home/zdhs0010/agentic/verl-agent-multi/data/multitask_data_test"
 TRAIN_DATA="${MULTITASK_DATA_DIR}/train.parquet"
-VAL_DATA="${MULTITASK_DATA_DIR}/test.parquet"
+VAL_DATA="${MULTITASK_DATA_DIR}/test_x32.parquet"
 
 STUDENT_MODEL="/data/home/zdhs0010/agentic/model/qwen-2.5-7b-it"
 ALFWORLD_TEACHER="/data/home/zdhs0010/agentic/model/alfworld-teacher-gigpo-qwen2.5-7b"
@@ -38,10 +38,10 @@ MATH_TEACHER="/data/home/zdhs0010/agentic/model/OpenThinker3-7B"
 
 
 python3 -m verl.trainer.main_ppo_multitask \
-    algorithm.adv_estimator=placeholder \
-    actor_rollout_ref.actor.kl_loss_type=full_reverse \
-    +actor_rollout_ref.actor.kl_topk_tokens=32 \
-    +actor_rollout_ref.actor.norm_to_one_for_kl=True \
+    algorithm.adv_estimator=opd \
+    actor_rollout_ref.actor.kl_loss_type=k1 \
+    +algorithm.opd.gamma=0.0 \
+    +algorithm.opd.reward_weight=0.0 \
     +multitask.enable=True \
     +multitask.batching_mode=sequential \
     +multitask.tasks.task0.name=alfworld \
@@ -60,16 +60,16 @@ python3 -m verl.trainer.main_ppo_multitask \
     data.max_prompt_length=2048 \
     data.max_response_length=16384 \
     data.filter_overlong_prompts=True \
-    data.truncation='error' \
+    data.truncation='middle' \
     data.return_raw_chat=True \
     +data.batching_mode=sequential \
     actor_rollout_ref.model.path=${STUDENT_MODEL} \
-    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.optim.lr=2e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=64 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=1 \
     actor_rollout_ref.actor.entropy_coeff=0.0 \
-    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.actor.kl_loss_coef=1 \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=True \
@@ -82,13 +82,14 @@ python3 -m verl.trainer.main_ppo_multitask \
     actor_rollout_ref.rollout.max_num_batched_tokens=$((2048 + 16384)) \
     actor_rollout_ref.rollout.enforce_eager=False \
     actor_rollout_ref.rollout.free_cache_engine=False \
-    actor_rollout_ref.rollout.val_kwargs.temperature=0.4 \
+    actor_rollout_ref.rollout.val_kwargs.temperature=1 \
+    actor_rollout_ref.rollout.val_kwargs.top_p=0.9 \
     actor_rollout_ref.rollout.val_kwargs.do_sample=True \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     actor_rollout_ref.actor.use_invalid_action_penalty=False \
     actor_rollout_ref.actor.invalid_action_penalty_coef=0.0 \
-    algorithm.use_kl_in_reward=False \
+    algorithm.use_kl_in_reward=True \
     env.env_name=multitask \
     env.seed=0 \
     env.max_steps=30 \
@@ -100,8 +101,8 @@ python3 -m verl.trainer.main_ppo_multitask \
     trainer.experiment_name="${exp_name}" \
     trainer.n_gpus_per_node=8 \
     trainer.nnodes=1 \
-    trainer.save_freq=20 \
-    trainer.test_freq=10 \
+    trainer.save_freq=40 \
+    trainer.test_freq=40 \
     trainer.total_epochs=1 \
     trainer.val_before_train=False \
     trainer.val_only=False \
